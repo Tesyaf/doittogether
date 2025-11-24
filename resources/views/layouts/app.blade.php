be7fb443475b486ff734716ac6f23474488341b5
amalaratri <amalaratri2303@gmail.com>
2025-11-24 23:01:10 +0700

    view user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,11 @@
 
     <title>{{ config('app.name', 'DoItTogether') }}</title>
 
+    {{-- Inter font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
     {{-- Favicon (opsional) --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
